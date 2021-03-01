e66ea51fb456203e1675b24b6994cbffcf512fa9
johnxferraro <johnxferraro@gmail.com>
2021-03-01 15:43:10 -0500

    app running changed backgroung, added search bar

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [input, setInput] = React.useState('Search Videos...')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello World</Text>
+      <TextInput style={styles.textInput}
+      onChangeText={text => setInput(text)}
+      value={input}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +19,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'white'
+  },
+  textInput: {
+    borderRadius: 4,
+    height: 40,
+    width: 150,
+    borderColor: 'black',
+    backgroundColor: 'white'
+  }
 });
